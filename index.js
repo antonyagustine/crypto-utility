780b0cd60b71e123fc130f99eb69c58ed3a7f7d4
Antony <antonyagustineraj@gmail.com>
2022-08-11 16:23:08 +0530

    Test commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function validURL(str) {
+  const pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-z\\d_]*)?$',
+    'i',
+  ); // fragment locator
+  return Boolean(pattern.test(str));
+}
+
+// Available region list
+const regions = {
+  NA: { cma: 'https://api.contentstack.io', cda: 'https://cdn.contentstack.io', name: 'NA' },
+  EU: { cma: 'https://eu-api.contentstack.com', cda: 'https://eu-cdn.contentstack.com', name: 'EU' },
+  'AZURE-NA': { cma: 'https://azure-na-api.contentstack.com', cda: 'https://azure-na-cdn.contentstack.com', name: 'AZURE-NA' },
+};
