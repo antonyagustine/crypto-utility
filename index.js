f81602cadaad803baad8d349660fcb1eb7b19f8b
Antony <antonyagustineraj@gmail.com>
2022-08-11 16:42:30 +0530

    Pattern update

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function validURL(str) {
   const pattern = new RegExp(
     '^(https?:\\/\\/)?' + // protocol
-    '(([a-z0-9\-]+\.){1,2}[a-z]{2,}|' + // domain name
+    // Actual code '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '(([a-z0-9A-Z]\.)*[a-z0-9-]+\.([a-z0-9]{2,24})+(\.co\.([a-z0-9]{2,24})|\.([a-z0-9]{2,24}))|' + // domain name
     '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
     '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
     '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
