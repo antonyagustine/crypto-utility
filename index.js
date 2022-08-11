2bbb5247c44b16f7ba100d64211c149af21d3e9d
Antony <antonyagustineraj@gmail.com>
2022-08-11 16:46:26 +0530

    Escape character added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function validURL(str) {
   const pattern = new RegExp(
     '^(https?:\\/\\/)?' + // protocol
     // Actual code '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-    '(([a-z0-9A-Z]\.)*[a-z0-9-]+\.([a-z0-9]{2,24})+(\.co\.([a-z0-9]{2,24})|\.([a-z0-9]{2,24}))|' + // domain name
+    '(([a-z0-9A-Z]\.)*[a-z0-9-]+\.([a-z0-9]{2,24})+(\.co\.([a-z0-9]{2,24})|\\.([a-z0-9]{2,24}))|' + // domain name
     '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
     '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
     '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
