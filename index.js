359ab4017435ed4255f40de4e130cb50a6e4b1d4
Antony <antonyagustineraj@gmail.com>
2022-09-07 17:07:10 +0530

    RegExp corrections

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function validURL(str) {
   const pattern = new RegExp(
     '^(https?:\\/\\/)?' + // protocol
-      '(([a-z0-9A-Z]\\.)*[a-z0-9-]+\\.([a-z0-9]{2,})+(\\.[a-z]{2,}\\.([a-z0-9]{2,})|\\.([a-z0-9]{2,}))|' + // domain name
+    '(([a-z0-9A-Z]\\.)*[a-z0-9-]+\\.([a-z0-9]{2,})+(\\.[a-z]{2,}\\.([a-z]{2,})|\\.([a-z]{2,}))|' + // domain name
       '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
       '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
       '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
